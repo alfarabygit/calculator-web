e7ac78627e02118f882a16f235726df9d3f0380c
Alfaraby Indra <alfarabyindra@gmail.com>
2023-08-05 23:06:01 +0700

    change several codes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const display = document.querySelector(".display");
+/*const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
 const specialChars = ["%", "*", "/", "-", "+", "="];
 let output = "";
@@ -24,3 +24,27 @@ const calculate = (btnValue) => {
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 });
+*/
+
+// calculator logic
+(function () {
+  let screen = document.querySelector(".screen");
+  let buttons = document.querySelectorAll(".btn");
+  let clear = document.querySelector(".btn-clear");
+  let equal = document.querySelector(".btn-equal");
+
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      let value = e.target.dataset.num;
+      screen.value += value;
+    });
+  });
+  equal.addEventListener("click", function (e) {
+    let answer = eval(screen.value);
+    screen.value = answer;
+  });
+
+  clear.addEventListener("click", function (e) {
+    screen.value = "";
+  });
+})();
